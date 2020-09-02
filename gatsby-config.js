73d460f84c93a6d7c7c901207a2abd51f052f775
Surya <a1b2c3d4config@gmail.com>
2020-09-02 22:16:06 +0530

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,15 @@
 require(`dotenv`).config({
   path: `.env`,
-})
+});
 
-const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE;
 
 module.exports = {
   siteMetadata: {
     // You can overwrite values here that are used for the SEO component
     // Of course you can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
+    siteTitleAlt: `My Portfolio`,
   },
   plugins: [
     {
@@ -33,18 +33,6 @@ module.exports = {
         background_color: `#141821`,
         theme_color: `#f6ad55`,
         display: `standalone`,
-        icons: [
-          {
-            src: `/android-chrome-192x192.png`,
-            sizes: `192x192`,
-            type: `image/png`,
-          },
-          {
-            src: `/android-chrome-512x512.png`,
-            sizes: `512x512`,
-            type: `image/png`,
-          },
-        ],
       },
     },
     `gatsby-plugin-offline`,
@@ -58,4 +46,4 @@ module.exports = {
       },
     },
   ].filter(Boolean),
-}
+};
